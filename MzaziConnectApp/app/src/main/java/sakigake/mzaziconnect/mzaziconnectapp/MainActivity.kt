3e44b27b820eff80e77fdd06b10eba0198a6b8f5
Nakakandebr <bridgetnakakandea@gmail.com>
2023-09-16 09:04:36 +0300

    created a mobile boiler plate

---
+++ MzaziConnectApp/app/src/main/java/sakigake/mzaziconnect/mzaziconnectapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package sakigake.mzaziconnect.mzaziconnectapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
