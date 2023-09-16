afc6227eab175c5bbe0caca4592b917a791173b2
Nakakandebr <bridgetnakakandea@gmail.com>
2023-09-16 11:32:46 +0300

    added a boiler plate

---
+++ MzaziConnectApplication/app/src/main/java/sakigake/mzaziconnect/mzaziconnectapplication/MainActivity.kt
@@ -0,0 +1,11 @@
+package sakigake.mzaziconnect.mzaziconnectapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
