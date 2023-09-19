e1d9aa9a569ceb504edc4fb087ffa3f317f9a206
Nakakandebr <bridgetnakakandea@gmail.com>
2023-09-19 16:31:43 +0300

    created screen for adding assignment

--- MzaziConnectApplication/app/src/main/java/sakigake/mzaziconnect/mzaziconnectapplication/ui/AddAssignment.kt
+++ MzaziConnectApplication/app/src/main/java/sakigake/mzaziconnect/mzaziconnectapplication/ui/AddAssignment.kt
@@ -3,10 +3,13 @@ package sakigake.mzaziconnect.mzaziconnectapplication.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import sakigake.mzaziconnect.mzaziconnectapplication.R
+import sakigake.mzaziconnect.mzaziconnectapplication.databinding.AddAssignmentBinding
 
 class AddAssignment : AppCompatActivity() {
+    lateinit var binding :AddAssignmentBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.add_assignment)
+        binding= AddAssignmentBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
