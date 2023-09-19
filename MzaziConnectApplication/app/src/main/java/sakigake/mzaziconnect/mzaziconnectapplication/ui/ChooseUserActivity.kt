c4bc3d5a5d4f299b6ba7f85d034632180448b52e
sakigake <mzaziconnect@gmail.com>
2023-09-19 15:33:55 +0300

    added splashscreen and choosing user screen

---
+++ MzaziConnectApplication/app/src/main/java/sakigake/mzaziconnect/mzaziconnectapplication/ui/ChooseUserActivity.kt
@@ -0,0 +1,30 @@
+package sakigake.mzaziconnect.mzaziconnectapplication.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import sakigake.mzaziconnect.mzaziconnectapplication.R
+import sakigake.mzaziconnect.mzaziconnectapplication.databinding.ActivityChooseUserBinding
+
+class ChooseUserActivity : AppCompatActivity() {
+    lateinit var binding: ActivityChooseUserBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityChooseUserBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        onResume()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.btnparent.setOnClickListener {
+            val intent = Intent(this@ChooseUserActivity, ChooseUserActivity::class.java)
+            startActivity(intent)
+        }
+        binding.btnteacher.setOnClickListener {
+            val intent = Intent(this@ChooseUserActivity, ChooseUserActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
---
+++ MzaziConnectApplication/app/src/main/java/sakigake/mzaziconnect/mzaziconnectapplication/ui/MainActivity.kt
@@ -0,0 +1,25 @@
+package sakigake.mzaziconnect.mzaziconnectapplication.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import sakigake.mzaziconnect.mzaziconnectapplication.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        onResume()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.btnstart1.setOnClickListener {
+            val intent = Intent(this@MainActivity, ChooseUserActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
